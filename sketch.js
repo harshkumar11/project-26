dcc439ef6ff79a342f38c40eb63000bd62d06bc9
harshkumar11 <72192474+harshkumar11@users.noreply.github.com>
2020-12-20 15:24:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+/*according to various Internet sources, 
+including these by Mental_Floss and io9, velvet 
+cakes of the past were made with baking soda and 
+either vinegar or buttermilk. When the baking soda
+ combined with either of these two ingredients, it 
+would bubble up and cause the cake to have a fluffy and smooth texture.*/
+
+
+
+
+
+
+
+
+
+
+/* 
+
+
+TIP #1 ON HOW TO MAKE BOX MIX TASTE HOMEMADE
+Start with the best box mix possible. I’m a Duncan Hines girl myself. The price is a little bit
+ more expensive than the super cheap box mix but I believe you get what you pay for. Not to mention Duncan 
+ Hines are often on sale so stock up on your top flavors and enjoy all the funny looks you’ll get at check out. 
+the best brand for doctored box mix recipes
+My favorite Duncan Hines box mix flavors are Red Velvet, Triple Chocolate, 
+White Cake (for making WASC) and strawberry. Sometime I can’t find duncan hines
+ strawberry so I’ll go with Betty Crocker. These are all recipes that take a ton of time to make from scratch and are the most requested flavors from my clients. For the chocolate version of this recipe check out my Chocolate WASC Recipe
+
+
+
+
+
+
+
+
+
+
+
+TIP #2 ON HOW TO MAKE BOX MIX TASTE HOMEMADE
+Usually a box mix has the following ingredients listed on the back that they want you 
+to add. Eggs, water and oil. This is smart on their end because pretty much everyone 
+has these ingredients on hand already.
+he thing about these ingredients though, they don’t add much to
+ the cake as far as flavor goes. 
+replace water with milk for more flavor and moisture. If you’re making red
+ velvet you can use buttermilk. 
+Replace the oil with melted butter for more flavor and
+ homemade texture
+For chocolate cakes, try replacing the water with cooled coffee. 
+The coffee intensifies the chocolate flavor! Get the recipe Chocolate WASC
+
+
+
+TIP #3 ON HOW TO MAKE BOX MIX TASTE HOMEMADE
+You might have heard of WASC (white almond sour cream cake) 
+and wondered what it is. It’s basically the best doctored box mix recipe of all time. The taste, 
+texture and flavor of this cake is TO DIE FOR. It’s no wonder its the recipe of choice when you need a 
+perfect white cake for so many bakers. 
+
+close up of WASC white almond sour cream cake, the best doctored box mix recipe for white cake
+How to make WASC
+
+Start with one box of white Duncan Hines mix
+Add in one cup of AP flour
+1 cup of granulated sugar
+1/4 tsp salt
+1 cup sour cream   
+1/2 cup melted butter  
+1 cup of water or milk  
+4 egg whites 
+1 tsp almond extract  
+Place all of your ingredients into the bowl of your stand
+ mixer (or you can mix by hand) and stir on low to combine until just moistened.
+  Then bump up to medium speed and mix for 2 minutes to develop the texture and structure of the cake. 
+
+Pour your batter into your prepared pans. I like to use a homemade pan
+ release (cake goop). The best homemade pan release there is! Bake at 350ºF 
+ for 35-40 minutes until a toothpick comes out from the center cleanly. 
+
+Let cool completely before decorating. For more info on 
+decorating your first cake, check out my free training on how to make your first cake. 
+
+TIP #4 ON HOW TO MAKE BOX MIX TASTE HOMEMADE
+Make your buttercream from scratch. I know this sounds 
+counter-intuitive because we’re trying to go for easy here. Maybe you’ve never made homemade 
+buttercream before and you’re intimidated. Well let me put your mind at ease. 
+
+My easy buttercream recipe is so simple, it may as well be cheating. Simply place 
+all your ingredients in a bowl, whip it until it’s light and fluffy. Then let it mix on
+ low with the paddle attachment for 10 minutes to get rid of bubbles.
+
+easy buttercream frosting
+Voila, perfect easy buttercream frosting. 
+
+And way, way, WAY better than that frosting from a can. What even IS that?
+
+Other great frosting options for you
+
+Stable cream cheese frosting
+Stabilized whipped cream (ok for one tier cakes with no fondant)
+Chocolate ganache
+American buttercream
+TIP #5 ON HOW TO MAKE BOX MIX TASTE HOMEMADE
+Go crazy with some yummy fillings! You might not want to make your cake from 
+scratch but most fillings for a cake can be made from scratch and they are super simple. 
+
+lemon curd recipe
+Try out these tasty fillings for your cake
+
+Strawberry reduction
+Lemon curd
+Marion berry buttercream
+Coconut pecan filling
+See, that wasn’t so hard was it? You’re on your way to making a delicious cake
+ made from a box. Do you have some tips on how to make your box mix taste homemade? Leave them in the comments!*/
